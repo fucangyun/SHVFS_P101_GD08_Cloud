914c439b1d4a374e5db66f6d6bd7410cc708bc26
fucangyun <954468261@qq.com>
2022-10-20 19:47:20 +0800

    update

---
+++ Classes/C04/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Q2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region
+
+            #region
+            /*            while (true)
+                        {
+                            float num1 = float.Parse(Console.ReadLine());
+                            float num2 = float.Parse(Console.ReadLine());
+                            Console.WriteLine($"{num1} ÷ {num2} = {num1 / num2}");
+                            //Console.WriteLine(num1 / num2);
+                            Console.WriteLine("Press anything to clear the screen...");
+                            Console.ReadLine(); 
+                            Console.Clear();
+                        }*/
+            #endregion
+            /*bool flag = 1 < 0;
+            Console.WriteLine(flag);*/
+
+            /*int num1 = 1220;
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            while (num2 != num1)
+            {
+                if (num2 > num1)
+                {
+                    Console.WriteLine($"too high");
+                }
+                if (num2 < num1)
+                {
+                    Console.WriteLine($"too low");
+                }
+                num2 = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"exactly!");
+            Console.ReadLine();*/
+
+            //Ctrl+R twice, change a variable's name all 
+            //'&&' calculates before '||' 
+
+            /*var rand = new Random();
+            var randombool1 = Convert.ToBoolean(rand.Next(2));//generate an int less than 2, than convert it to bool
+            var randombool2 = Convert.ToBoolean(rand.Next(2));
+            var randombool3 = Convert.ToBoolean(rand.Next(2));
+            Console.WriteLine("First is {0}, Second is {1}, Third is {2}",randombool1, randombool2, randombool3);*/
+
+            #endregion
+
+
+
+
+        }
+    }
+}       
